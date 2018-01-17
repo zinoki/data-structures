4458d00fd708861aedf9405056ab100a68889700
Zachary <zinouye@gmail.com>
2018-01-17 11:47:22 -0800

    Keeping track of count

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,21 @@
 var Stack = function() {
   var someInstance = {};
+  var count = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    count++;
   };
 
   someInstance.pop = function() {
+    count--;
   };
 
   someInstance.size = function() {
+    return count;
   };
 
   return someInstance;
