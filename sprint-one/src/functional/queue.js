a46c70a0c52127e38b40a9ccbc812cce0769e35e
Zachary <zinouye@gmail.com>
2018-01-17 12:00:55 -0800

    completed shared behavior of queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,5 +1,6 @@
 var Queue = function() {
   var someInstance = {};
+  var count = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
@@ -7,12 +8,19 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    someInstance[count] = value;
+    count++;
   };
 
   someInstance.dequeue = function() {
+    var temp = someInstance[count-1];
+    someInstance[count] = undefined;
+    count--;
+    return temp;
   };
 
   someInstance.size = function() {
+    return count < 0 ? 0 : count;
   };
 
   return someInstance;
