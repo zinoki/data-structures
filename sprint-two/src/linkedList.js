c0019fa5b7af650b496fddb00c3db6fd86dcda7e
zinoki <zinouye@gmail.com>
2018-01-18 13:09:31 -0800

    Finished linkedlist.js

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,32 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if (list.head == null) {
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      list.tail.next = newNode;
+      list.tail = newNode;
+    }
   };
 
   list.removeHead = function() {
+    var deletedHead = list.head;
+    list.head = deletedHead.next;
+    return deletedHead.value;
+    
   };
 
   list.contains = function(target) {
+    var currentHead = list.head;
+    while (currentHead) {
+      if (currentHead.value === target) {
+        return true;
+      }
+      currentHead = currentHead.next;
+    };
+    return false;
   };
 
   return list;
