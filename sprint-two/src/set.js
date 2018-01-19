aa92d6cc26f499f2fb6e2ddf84e76151802bf26c
zinoki <zinouye@gmail.com>
2018-01-18 21:18:02 -0800

    Completed set.js

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,21 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage[item] = item;
 };
 
 setPrototype.contains = function(item) {
+  return this._storage[item] ? true : false;
 };
 
 setPrototype.remove = function(item) {
+  delete this._storage[item];
 };
 
 /*
