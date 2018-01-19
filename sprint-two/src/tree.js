ebba46be88ccfc83eb668b11f8fe66ce4d7f1956
zinoki <zinouye@gmail.com>
2018-01-18 17:58:38 -0800

    Completed trees and passing tests

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -11,10 +11,20 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
-  this.children.push({value: value})
+  this.children.push(new Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  } else {
+    for (var i = 0; i < this.children.length; i++) {
+      if (this.children[i].contains(target)) {
+        return true;
+      }
+    }
+  } 
+  return false;
 };
 
 
