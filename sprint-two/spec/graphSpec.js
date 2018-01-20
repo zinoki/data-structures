51a484ffdf05227183b894efc9107e24c90b62ab
zinoki <zinouye@gmail.com>
2018-01-20 10:19:31 -0800

    added test for graphSpec.js

--- sprint-two/spec/graphSpec.js
+++ sprint-two/spec/graphSpec.js
@@ -68,4 +68,16 @@ describe('graph', function() {
     expect(graph.hasEdge(3, 5)).to.equal(true);
     expect(graph.hasEdge(5, 5)).to.equal(true);
   });
+    
+  it('should check if recently deleted node had any previous edges from before', function () {
+    graph.addNode(4);
+    graph.addNode(5);
+    graph.addEdge(5, 4);
+    expect(graph.hasEdge(4, 5)).to.equal(true);
+    graph.removeNode(5);
+    expect(graph.hasEdge(4, 5)).to.equal(false);
+    graph.addNode(5);
+    expect(graph.hasEdge(4, 5)).to.equal(false);
+  });
+
 });
